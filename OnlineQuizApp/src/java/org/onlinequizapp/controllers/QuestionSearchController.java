ce30067e7e4c20d443857676cf9d2a5f2b6337fc
tonynguyen2512 <nguyenhathien2512@gmail.com>
2021-07-25 17:26:42 +0700

    Ques Search Error

--- OnlineQuizApp/src/java/org/onlinequizapp/controllers/QuestionSearchController.java
+++ OnlineQuizApp/src/java/org/onlinequizapp/controllers/QuestionSearchController.java
@@ -1,24 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.onlinequizapp.controllers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.onlinequizapp.daos.QuestionDAO;
+import org.onlinequizapp.dtos.QuestionDTO;
 
-/**
- *
- * @author User-PC
- */
 @WebServlet(name = "QuestionSearchController", urlPatterns = {"/QuestionSearchController"})
 public class QuestionSearchController extends HttpServlet {
+
+    private static final String SUCCESS = "question.jsp";
+    private static final String QUESTION = "questionAdd.jsp";
+    private static final String ERROR = "404.html";
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -32,21 +32,29 @@ public class QuestionSearchController extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet QuestionSearchController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet QuestionSearchController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        String url = ERROR;
+        String check = request.getParameter("check");
+        String search = request.getParameter("search");
+        if (check.equals("Search")) {
+            try {
+                QuestionDAO dao = new QuestionDAO();
+                List<QuestionDTO> list = dao.getListQ(search);
+                if (list != null) {
+                    request.setAttribute("LIST_QUESTION", list);
+                    url = SUCCESS;
+                }
+            } catch (SQLException e) {
+                log("Error at ClassSearchController: " + e.toString());
+            } finally {
+                request.getRequestDispatcher(url).forward(request, response);
+            }
+        } else {
+            request.setAttribute("LIST_QUESTION_ERROR", "ERROR at ClassSearchController");
+            url = ERROR;
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
